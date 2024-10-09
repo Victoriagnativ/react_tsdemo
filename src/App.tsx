12a305e8fc7a8000ead67f29b65563220c41bfdd
Victoriagnativ <victoria.paluk@gmail.com>
2024-10-09 15:13:35 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React, {FC} from 'react';
 import './App.css';
+import Character from "./components/character/Character";
 
 const App: FC  = ()=> {
   return (
     <>
+      <Character name ={'bart'}
+                 image ={'https://w7.pngwing.com/pngs/229/576/png-transparent-bart-simpson-illustration-bart-simpson-animation-bart-simpson-free-hand-cartoon-graphic-arts-thumbnail.png'}
+      />
+      <Character name ={'homer'}
+                 image ={'https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png'}
+      />
+        <Character name ={'marge'}
+                   image ={'https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png'}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but also
+            the leap into electronic typesetting, remaining essentially unchanged.
+        </Character>
     </>
   );
 }
---
+++ src/components/character/Character.tsx
@@ -0,0 +1,22 @@
+import React, {FC, ReactNode,} from 'react';
+
+interface IProps{
+    name:string;
+    image:string;
+    // children?: React.ReactNode;
+}
+type  PropsWithChildren<T> = T & { children?: ReactNode };
+
+const Character:FC<PropsWithChildren<IProps>> = ({name,image, children}) => {
+
+    return (
+        <div>
+            <h2>{name}</h2>
+            <img src = {image} alt={name}/>
+            <p>{children}</p>
+
+        </div>
+    );
+};
+
+export default Character;
